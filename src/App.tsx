9e476ad7e7b3322adbdfa982080244f71bc14aec
MakoDoli <makodolidze@hotmail.com>
2023-05-13 17:53:16 +0400

    launch countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contacts from "./Contacts";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [days, setDays] = useState<number | null>(null);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    //set countdown time amount in seconds
+    const timeSet = 1209600000;
+    const countdownTime = new Date().getTime() + timeSet;
+    const countDown = setInterval(function () {
+      // Get the current time
+      const now = new Date().getTime();
+
+      // Find the distance between now and the countdown time
+      const distance = countdownTime - now;
+
+      // Calculate days, hours, minutes, and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setDays(days);
+      setHours(hours);
+      setMinutes(minutes);
+      setSeconds(seconds);
+
+      if (distance <= 0) {
+        clearInterval(countDown);
+        console.log("Countdown finished!");
+      }
+    }, 1000);
+    return () => clearInterval(countDown);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="main">
+        <h1> WE'RE LAUNCHING SOON</h1>
+        <div className="count">
+          <div className="time">
+            <div className="upper"></div>
+            <div className="number">{days}</div>
+            <div className="dot dot-1"></div>
+            <div className="dot dot-2"></div>
+            <div className="line"></div>
+            <div className="lower"></div>
+          </div>
+          <div className="time">
+            <div className="upper"></div>
+            <div className="number">{hours}</div>
+            <div className="dot dot-1"></div>
+            <div className="dot dot-2"></div>
+            <div className="line"></div>
+            <div className="lower"></div>
+          </div>
+          <div className="time">
+            <div className="upper"></div>
+            <div className="number">{minutes}</div>
+            <div className="dot dot-1"></div>
+            <div className="dot dot-2"></div>
+            <div className="line"></div>
+            <div className="lower"></div>
+          </div>
+          <div className="time">
+            <div className="upper"></div>
+            <div className="number">{seconds}</div>
+            <div className="dot dot-1"></div>
+            <div className="dot dot-2"></div>
+            <div className="line"></div>
+            <div className="lower"></div>
+          </div>
+        </div>
+        <div className="grid">
+          <p className="parts">days</p>
+          <p className="parts">hours</p>
+          <p className="parts">minutes</p>
+          <p className="parts">seconds</p>
+        </div>
+        <Contacts />
+      </div>
+    </main>
   );
 }
 
